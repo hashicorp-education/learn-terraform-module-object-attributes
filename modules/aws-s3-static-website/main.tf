7738ae0a70d4de745c6edc777bc436358779afc3
ssungz789 <nin7344@naver.com>
2024-07-11 13:35:08 +0900

    403 arn reference change in s3 policy due to access denial

--- modules/aws-s3-static-website/main.tf
+++ modules/aws-s3-static-website/main.tf
@@ -60,8 +60,11 @@ resource "aws_s3_bucket_policy" "web" {
         Principal = "*"
         Action    = "s3:GetObject"
         Resource = [
-          aws_s3_bucket.web.arn,
+          "${aws_s3_bucket.web.arn}",
           "${aws_s3_bucket.web.arn}/*",
+        # Resource = [
+        #   aws_s3_bucket.web.arn,
+        #   "${aws_s3_bucket.web.arn}/*",
         ]
       },
     ]
